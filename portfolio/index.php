c6635606f55f2d98cf62a023424b77136a92619e
jonaderm <jonasdrmul@hotmail.com>
2017-06-02 11:09:55 +0200

    portfolio test mailer

---
+++ portfolio/index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Graphic Design - Jonas Dermul</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/portfolio.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-left toggle"><i class="fa fa-times"></i></a>
+            <li class="sidebar-brand">
+                <a href="#top" onclick=$("#menu-close").click();>Home&lowbar;</a>
+            </li>
+            <br>
+            <li>
+                <a href="#about" id="linkabout" onclick=$("#menu-close").click();>Over Mij&lowbar;</a>
+            </li>
+            <li>
+                <a href="#portfolio" id="linkportfolio" onclick=$("#menu-close").click();>Portfolio&lowbar;</a>
+            </li>
+            <li>
+                <a href="#contact" id="linkcontact" onclick=$("#menu-close").click();>Contact&lowbar;</a>
+            </li>
+            
+            <li id="copy">
+                <p>&copy; Dermul Jonas&lowbar; 2017&lowbar;</p>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Header -->
+    <header id="top" class="header">
+        <div class="container picheader">
+            <div class="row">
+                    <div class="col-lg-2 col-md-0 col-xs-0">
+                    </div>
+                
+                    <div class="col-lg-4 col-md-6 col-xs-12">
+
+                        <p class="name">Name&lowbar;</p>
+                        <h1 id='jonas'>&sol;JONAS</h1>
+                        <h1 id='dermul'>DERMUL</h1>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-xs-0">
+                        <p class="select">Select colleague.</p>
+                    </div>
+                
+                <div class="col-lg-2 col-md-0">
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-lg-2 col-md-0">
+                </div>
+
+                <div class="col-lg-10 col-md-12">
+                    <a id="knowme" href="#about">Get to know me<br><i class="fa fa-angle-down bounce"></i></a>
+                </div>
+                
+                <div class="col-lg-2 col-md-0">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About -->
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                </div>
+                
+                <div class="col-lg-8 text-center">
+                    <p class="tekst">
+                        &lt; Mijn naam is Dermul Jonas en ik ben een 22-jarige grafisch ontwerper met een passie voor design, illustraties en typografie &gt; <br> <br> &lt; Ik ben een creatief, gedreven en flexibel persoon met als doel te werken voor een grafisch bedrijf en mezelf nog meer te ontwikkelen als ontwerper &gt; 
+                    </p>
+                </div>
+                
+                <div class="col-lg-2">
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Titel Portfolio -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                </div>
+                
+                <div class="col-lg-8">
+                    <h2 class="tportfolio">Portfolio&lowbar;</h2>
+                </div>
+                
+                <div class="col-lg-2">
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <section class="no-padding">
+        <div class="container-fluid"> 
+            <div class="row no-gutter popup-gallery">
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/1.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/2.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/2.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/3.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/3.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/4.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/4.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/5.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/6.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/6.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/1.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="foto col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/2.jpg" class="portfolio-box">
+                        <img src="img/portfolio/thumbnails/12.png" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <section id="contact" class="footer">
+        <div class="container picfooter">
+            <div class="row">
+                
+                <div class="col-lg-2">
+                </div>
+                
+                <div class="col-lg-8">
+                    <h2 class="tcontact">Contact&lowbar;</h2>
+                </div>
+                
+                <div class="col-lg-2">
+                </div>
+                
+                <div class="col-lg-3 col-md-1 col-sm-1">
+                </div>
+                
+                <div class="col-lg-4 col-md-0 col-sm-0">
+                </div>
+                
+                <div class="col-lg-3 col-md-10 col-sm-10">
+                    <div class="col-lg-12 col-md-4 col-sm-6">
+                    <p class="address">Address&lowbar;</p>
+                    <p class="tinfocontact">
+                        Weststraat 21c<br>
+                        9160 Lokeren<br>
+                        +32 476 56 09 04
+                    </p>
+                    </div>
+                    <div class="col-lg-12 col-md-8 col-sm-6">
+                        
+                    <p class="contactmij">Contact mij&lowbar;</p>
+                        
+                        <?=$thankYou ?>
+                        
+                        <form id="contact_form" action="php/contact.php" method="POST" enctype="multipart/form-data">
+                            <div class="row">
+                                <label for="name"></label><br />
+                                <input id="name" class="input" name="sender" type="text" placeholder="Naam" value=""/><br />
+                            </div>
+                            <div class="row">
+                                <label for="email"></label><br />
+                                <input id="email" class="input" name="senderEmail" type="text" placeholder="E-mail" value=""/><br />
+                            </div>
+                            <div class="row">
+                                <label for="message"></label><br />
+                                <textarea id="message" class="input" name="message" placeholder="Tekstje" rows="4"></textarea><br />
+                            </div>
+                            <input id="submit_button" action="submit" type="submit" value="&lt;Verzenden&gt;"/>
+                        </form>
+                </div>
+                </div>
+                    
+                
+                <div class="col-lg-2 col-md-1 col-sm-1">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script>
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+
+</body>
+
+</html>
